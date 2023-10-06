af6ccb9acf4fdd6f379322b933642e2b0c9de476
venskalalihatu <venskalalihatu6@gmail.com>
2023-10-06 12:15:48 +0700

    install cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 import PackageRouter from "./Routes/packageroutes.js";
 
 const app = express();
@@ -8,6 +9,7 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(PackageRouter);
 
