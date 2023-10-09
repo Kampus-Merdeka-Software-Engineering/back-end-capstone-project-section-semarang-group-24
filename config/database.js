93e1dd04eb3a2c09b0d90e8e6d58f200bab98d7d
venskalalihatu <venskalalihatu6@gmail.com>
2023-10-09 11:58:45 +0700

    edit railway

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize ("sendpackages_db", "root", "venska18-", {
-    host: "localhost",
+const db = new Sequelize ("railway", "root", "f0gThAUFvkmMDkeiah1f", {
+    host: "containers-us-west-88.railway.app",
+    port: "5590",
     dialect: "mysql",
 });
+
+
 
 db.authenticate().then(() => `Database is connected`).catch(error => console.log(error));
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import PackageRouter from "./Routes/packageroutes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -13,4 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(PackageRouter);
 
-app.listen(PORT, () => console.log(`app running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`app running on port ${PORT}`));
+
+app.listen(PORT, "0.0.0.0", function () {});
